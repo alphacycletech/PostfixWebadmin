ed0e3e7095c9c602eac5982bfd765081eca3bbf2
alphacycletech <90429352+alphacycletech@users.noreply.github.com>
2024-12-02 16:47:34 +0800

    Add files via upload

--- trace.py
+++ trace.py
@@ -1,51 +1,54 @@
 import pandas as pd
 import sqlite3
+import glob
 
 conn = sqlite3.connect('maillog.db')
 
 conn.execute("DROP TABLE log")
 conn.execute("CREATE TABLE log (Date VARCHAR(20),Server VARCHAR(100),QueueId VARCHAR(100),EmailId VARCHAR(100),FromUser VARCHAR(100),ToUser VARCHAR(100),Status VARCHAR(50))")
 
-df = pd.read_csv('/var/log/mail.log', sep='delimiter', header=None, engine='python')
-log_array=df.values.tolist()
+for file in glob.glob('/var/log/mail.log*'):
+    if('gz' not in file):
+        df = pd.read_csv(file, sep='delimiter', header=None, engine='python')
+        log_array=df.values.tolist()
 
-def replaceMonth(Month):
-    if(Month=='Jan'):
-        newMonth='01'
-    elif(Month=='Feb'):
-        newMonth='02'
-    elif(Month=='Mar'):
-        newMonth='03'
-    elif(Month=='Apr'):
-        newMonth='04'
-    elif(Month=='May'):
-        newMonth='05'
-    elif(Month=='Jun'):
-        newMonth='06'
-    elif(Month=='Jul'):
-        newMonth='07'
-    elif(Month=='Aug'):
-        newMonth='08'
-    elif(Month=='Sep'):
-        newMonth='09'
-    elif(Month=='Oct'):
-        newMonth='10'
-    elif(Month=='Nov'):
-        newMonth='11'
-    elif(Month=='Dec'):
-        newMonth='12'
+        def replaceMonth(Month):
+            if(Month=='Jan'):
+                newMonth='01'
+            elif(Month=='Feb'):
+                newMonth='02'
+            elif(Month=='Mar'):
+                newMonth='03'
+            elif(Month=='Apr'):
+                newMonth='04'
+            elif(Month=='May'):
+                newMonth='05'
+            elif(Month=='Jun'):
+                newMonth='06'
+            elif(Month=='Jul'):
+                newMonth='07'
+            elif(Month=='Aug'):
+                newMonth='08'
+            elif(Month=='Sep'):
+                newMonth='09'
+            elif(Month=='Oct'):
+                newMonth='10'
+            elif(Month=='Nov'):
+                newMonth='11'
+            elif(Month=='Dec'):
+                newMonth='12'
     
-    return newMonth
+            return newMonth
 
-for x in log_array:
-    myDate=replaceMonth(x[0][:3])+"-"+x[0][4:15]
-    myServer=x[0][16:25]
-    myQueueId='' if x[0].find('[')==-1 else x[0][(x[0].find('['))+1:(x[0].find(']'))]
-    myEmailId='' if x[0].find(']')==-1 else x[0][x[0].find(']')+3:x[0].find(']')+14]
-    myFrom='' if x[0].find('from=')==-1 else x[0][(x[0].find('from='))+6:(x[0].find('>,'))]
-    myTo='' if x[0].find('to=')==-1 else x[0][(x[0].find('to='))+4:(x[0].find('>,'))]
-    myStatus='' if x[0].find('status=')==-1 else x[0][(x[0].find('status='))+7:(x[0].find('('))-1]
-    conn.execute("INSERT INTO log VALUES ('"+myDate+"','"+myServer+"','"+myQueueId+"','"+myEmailId+"','"+myFrom+"','"+myTo+"','"+myStatus+"')")
+        for x in log_array:
+            myDate=replaceMonth(x[0][:3])+"-"+x[0][4:15]
+            myServer=x[0][16:25]
+            myQueueId='' if x[0].find('[')==-1 else x[0][(x[0].find('['))+1:(x[0].find(']'))]
+            myEmailId='' if x[0].find(']')==-1 else x[0][x[0].find(']')+3:x[0].find(']')+14]
+            myFrom='' if x[0].find('from=')==-1 else x[0][(x[0].find('from='))+6:(x[0].find('>,'))]
+            myTo='' if x[0].find('to=')==-1 else x[0][(x[0].find('to='))+4:(x[0].find('>,'))]
+            myStatus='' if x[0].find('status=')==-1 else x[0][(x[0].find('status='))+7:(x[0].find('('))-1]
+            conn.execute("INSERT INTO log VALUES ('"+myDate+"','"+myServer+"','"+myQueueId+"','"+myEmailId+"','"+myFrom+"','"+myTo+"','"+myStatus+"')")
     #print(myEmailId)
     
 conn.commit()
