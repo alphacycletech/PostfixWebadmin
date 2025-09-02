c11eacf7fed2ed98dea3316e2f9c65e642119cef
alphacycletech <90429352+alphacycletech@users.noreply.github.com>
2025-09-02 22:01:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -24,13 +24,14 @@ if(!isset($_POST['password'])){
 }
 
 echo "<div align='center' style='margin-top:20vh'><img src='".$logo."' width='170vw'></div>";
-echo "<style>.font-md {font-size:12px} .font-lg {font-size:30px}</style>";
+echo "<style>.font-md {font-size:12px} .font-lg {font-size:30px} #checking_btn:hover {color:#6B6666 !important}</style>";
 echo "<form method='post'><table align='center' style='margin-top:2vh'>";
 echo "<tr><td align='center'><i class='fa fa-user font-lg'></i>&nbsp</td><td><input type='text' name='username' placeholder='Username' class='form-control font-md text-center' autofocus></td></tr>";
 echo "<tr><td align='center'><i class='fa fa-lock font-lg'></i>&nbsp</td><td><input type='password' name='password' placeholder='Password' class='form-control font-md text-center'></td></tr>";
 echo "<tr><td colspan=2><input type='submit' name='submit' value='LOGIN' class='btn btn-secondary font-md' style='width:100%'></td></tr>";
 echo "</table></form>";
 echo "<p align='center' style='color:grey' class='font-md'>".$sub_title."</p>";
+echo "<a href='checking.php' title='Check Dependencies' style='text-decoration:none;color:grey;font-size:2vw;position:absolute;bottom:1%;right:1%;z-index:10' id='checking_btn'><i class='fa fa-exclamation-circle'></i></a>";
    
 if(!isset($_POST['submit'])){
 }else{
@@ -45,8 +46,7 @@ if(!isset($_POST['submit'])){
 echo "<table style='background:pink' align='center'><tr height='50px'><td width='400px' align='center' style='border:1px solid red;color:red'>You've entered the wrong username or password!</td></tr></table>";
       }
 echo "<style>.footer {  position: fixed;right: 0;bottom: -3%;left: 0;padding: 1rem; style='background:rgba(0,0,0,0)';text-align: center;}</style>";
-echo "<pre>".shell_exec('python3 test.py')."</pre>";
-echo "</body>";
+echo "</body></table>";
 echo "<footer><p class='footer font-md'>".$footer."</p></footer>";
 
 function isMobileDevice() { 
